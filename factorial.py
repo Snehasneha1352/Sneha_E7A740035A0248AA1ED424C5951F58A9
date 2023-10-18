62fde752fbb46ac656ccd7789fe13d6af8f29526
Snehasneha1352 <148304694+Snehasneha1352@users.noreply.github.com>
2023-10-18 14:50:09 +0530

    Comitted code

---
+++ factorial.py
@@ -0,0 +1,25 @@
+num = 7
+
+factorial = 1
+
+if num < 0:
+  print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+  print("The factorianum = 7
+
+factorial = 1
+
+if num < 0:
+  print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+  print("The factorial of 0 is 1")
+else:
+  for i in range(1, num + 1):
+    factorial = factorial * i
+  print("The factorial of", num, "is", factorial)
+l of 0 is 1")
+else:
+  for i in range(1, num + 1):
+    factorial = factorial * i
+  print("The factorial of", num, "is", factorial)
+  
